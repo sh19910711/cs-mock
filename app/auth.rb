0baeba3b4149df2ecc3dea2e8373eaf80737d7d7
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-16 07:41:29 +0000

    auth: add /logout

--- app/auth.rb
+++ app/auth.rb
@@ -1,4 +1,5 @@
 class Auth < Base
   get('/login') { slim :auth_login, :layout => :layout_nosidebar }
   get('/register') { slim :auth_register, :layout => :layout_nosidebar }
+  get('/logout') { redirect '/' }
 end
