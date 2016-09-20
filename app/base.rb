d3bfca41aedb541701c4da29eaa77603b3104894
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-20 07:24:16 +0000

    base: add login_user helper

--- app/base.rb
+++ app/base.rb
@@ -1,5 +1,17 @@
 class Base < Sinatra::Base
+  enable :sessions
+
   configure :development do
     register Sinatra::Reloader
+  end
+
+  helpers do
+    def login_user
+      if user = User.find_by(login_id: session[:user_login_id])
+        user
+      else
+        halt 404
+      end
+    end
   end
 end
