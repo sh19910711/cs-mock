160759036c9ca0573bf81315d7087e2ea91ba198
Hiroyuki Sano <sh19910711@gmail.com>
2016-09-20 12:06:00 +0000

    screenshot: output as markdown

--- script/upload.bash
+++ script/upload.bash
@@ -1,6 +1,11 @@
+# upload
 clientid='3e7a4deb7ac67da'
 img=$1
 res=$(curl -sH "Authorization: Client-ID $clientid" -F "image=@$img" "https://api.imgur.com/3/upload")
 echo $res | grep -qo '"status":200' && link=$(echo $res | sed -e 's/.*"link":"\([^"]*\).*/\1/' -e 's/\\//g')
-echo -n "$img: "
-echo $link
+
+# output
+echo -n "![$img]("
+echo -n $link
+echo ")"
+echo ""
